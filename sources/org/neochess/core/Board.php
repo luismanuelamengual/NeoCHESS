4efeb6bfd04c610e0041dcd245ff70c6239a01c1
Luis Manuel Amengual <luismanuelamengual@gmail.com>
2016-07-25 15:19:13 -0300

    Agrego offset de casilleros

--- sources/org/neochess/core/Board.php
+++ sources/org/neochess/core/Board.php
@@ -4,6 +4,9 @@ namespace org\neochess\core;
 
 class Board
 {
+    const SQUARE_OFFSIDE = -1;
+    const SQUARE_EMPTY = 0;
+    
     const WHITE = 1;
     const BLACK = -1;
     
@@ -97,19 +100,64 @@ class Board
     const BLACK_CASTLE_SHORT = 4;
     const BLACK_CASTLE_LONG = 8;
     
+    private static $squareOffset = [
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1,  0,  1,  2,  3,  4,  5,  6,  7, -1, -1,
+        -1, -1,  8,  9, 10, 11, 12, 13, 14, 15, -1, -1,
+        -1, -1, 16, 17, 18, 19, 20, 21, 22, 23, -1, -1,
+        -1, -1, 24, 25, 26, 27, 28, 29, 30, 31, -1, -1,
+        -1, -1, 32, 33, 34, 35, 36, 37, 38, 39, -1, -1,
+        -1, -1, 40, 41, 42, 43, 44, 45, 46, 47, -1, -1,
+        -1, -1, 48, 49, 50, 51, 52, 53, 54, 55, -1, -1,
+        -1, -1, 56, 57, 58, 59, 60, 61, 62, 63, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+    ];
+    
     private $squares;
     private $epSquare;
     private $castleState;
     
+    public static function getOffsetSquare ($square, $horizontalOffset, $verticalOffset)
+    {
+        return self::$squareOffset[($square + 26) + ($verticalOffset * 8) + $horizontalOffset];
+    }
+    
     public function __construct ()
     {
-        $this->squares = [];
+        $this->squancres = [];
         $this->epSquare = -1;
         $this->castleState = 0;
     }
     
-    public function getSquare ($square)
+    public function getPiece ($square)
     {
         return $this->squares[$square];
+    }
+    
+    public function setPiece ($square, $piece)
+    {
+        $this->squares[$square] = $piece;
+    }
+    
+    public function getEpSquare ()
+    {
+        return $this->epSquare;
+    }
+    
+    public function setEpSquare ($square)
+    {
+        $this->epSquare = $square;
+    }
+    
+    public function getCastleState()
+    {
+        return $this->castleState;
+    }
+
+    public function setCastleState($castleState)
+    {
+        $this->castleState = $castleState;
     }
 }
